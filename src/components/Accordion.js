30cc815164ee673dff394fcf280be7ce2c14e511
@isaquesene <isaqueconcursos26@gmail.com>
2024-01-17 09:38:44 -0300

    feat: open accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -10,7 +10,8 @@ const Accordion = ({accordion}) => {
   const { question, answer } = accordion;
 
   return (
-    <div className='cursor-pointer'>
+    <div onClick={() => setIsOpen(!isOpen)} 
+      className='cursor-pointer'>
       <div className='bg-white border rounded-sm'>
         <div className='min-h-[60px] flex items-center justify-between px-[30px]'>
           <h6 className='h6'>{question}</h6>

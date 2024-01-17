6dd22fdc16d614ba479944d53fd3c32c346edc59
@isaquesene <isaqueconcursos26@gmail.com>
2024-01-17 09:46:47 -0300

    feat: tyle faq part 2

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -13,7 +13,11 @@ const Faq = () => {
     <section className='section pt-[480px] lb:pt-[280px]'>
       <div className='max-w-[768px] mx-auto lg:bg-faq bg-no-repeat bg-custom bg-center lg:h-[1160px] lg:pt-6'>
         {/* section title */}
-        <div className='section-title-group justify-start lg:justify-center -space-x-4 lg:max-w-[540px] mx-auto px-4 lg:px-0'>
+        <div className='section-title-group justify-start lg:justify-center -space-x-4 lg:max-w-[540px] mx-auto px-4 lg:px-0'
+          data-aos='fade-up'
+          data-aos-offset='200'
+          data-aos-delay='200'
+        >
           <img className='lg:hidden' src={icon} alt=''/>
           <h2 className='h2 section-title lg:mt-[100px]'>
             {title}

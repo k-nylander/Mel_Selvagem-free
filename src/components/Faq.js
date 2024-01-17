207caf2a59b6bc4a7b440f716e3a265276bf522f
@isaquesene <isaqueconcursos26@gmail.com>
2024-01-17 07:16:05 -0300

    feat: faq part 1

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// import data 
+import { faq } from '../data';
+
+// import components
+import Accordion from '../components/Accordion';
+
 const Faq = () => {
   return <div>Faq</div>;
 };

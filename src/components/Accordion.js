e1f0187eb23f89574a91816c27d577b8e1712f69
@isaquesene <isaqueconcursos26@gmail.com>
2024-01-17 08:11:37 -0300

    feat: accordion part 1

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Accordion = () => {
-  return <div>Accordion</div>;
+// import icons
+import { FaChevronCircleUp, FaChevronCircleDown } from 'react-icons/fa';
+
+const Accordion = ({accordion}) => {
+  //accordion state
+  const [ isOpen, setIsOpen ] = useState(false);
+  //destructure accordion
+  const { question, answer } = accordion;
+
+  return (
+    <div>
+      Accordion
+    </div>);
 };
 
 export default Accordion;

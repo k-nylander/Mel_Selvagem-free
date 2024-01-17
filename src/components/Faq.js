85ffd51fc32ec77c1263b099c6dd910a1ec0077d
@isaquesene <isaqueconcursos26@gmail.com>
2024-01-17 07:59:34 -0300

    feat: faq part 6

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -11,7 +11,7 @@ const Faq = () => {
   const { icon, title, accordions } = faq;
   return (
     <section className='section pt-[480px] lb:pt-[280px]'>
-      <div>
+      <div className='max-w-[768px] mx-auto lg:bg-faq bg-no-repeat bg-custom bg-center lg:h-[1160px] lg:pt-6 bg-pink-100'>
         {/* section title */}
         <div>
           <img className='lg:hidden' src={icon} alt=''/>
@@ -19,6 +19,12 @@ const Faq = () => {
             {title}
             <span className='text-primary-400'>?</span>
           </h2>
+        </div>
+        {/* accordion list */}
+        <div>
+          {accordions.map((accordion, idx)=>{
+            return <Accordion accordion={accordion} key={idx} />;
+          })}
         </div>
       </div>
     </section>

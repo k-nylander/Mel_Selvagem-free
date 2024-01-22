9af0b10551e5929ca4020ba38cbd1c6b4e97e251
@isaquesene <isaqueconcursos26@gmail.com>
2024-01-22 18:46:09 -0300

    feat: produto

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+// import data
+import { join } from '../data';
+
 const Join = () => {
-  return <div>Join</div>;
+  // destructure join data
+  const { image, title, subtitle, btnText } = join;
+  return (
+    <section>
+      <div className='container mx-auto'>
+        {/*  */}
+      </div>
+    </section>
+  );
 };
 
 export default Join;

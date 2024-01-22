c7ad6dbace4bd9855ffa835ced88aaa755afd5cc
@isaquesene <isaqueconcursos26@gmail.com>
2024-01-22 20:28:34 -0300

    feat footer final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,17 @@ const Footer = () => {
   //estrutura footer data
   const { logo, copyrightText } = footer;
   return (
-    <footer>
-      <div className='container mx-auto'>
+    <footer className='bg-neutral-500 h-[125px] md:h-[195px] px-[20px]'>
+      <div className='container mx-auto h-full flex justify-between items-center md:items-end md:pb-[50px]'
+        data-aos='fade-up'
+        data-aos-delay='200'
+      >
         {/* logo */}
         <a href='#'>
           <img src={logo} alt=''/>
         </a>
         {/* texto footer */}
-        <p>{copyrightText}</p>
+        <p className='text-neutral-300 text-sm'>{copyrightText}</p>
       </div>
     </footer>
   );

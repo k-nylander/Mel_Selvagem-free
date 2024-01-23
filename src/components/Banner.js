6039241796bf6de7f8537ee04809ed1856f7708a
@isaquesene <isaqueconcursos26@gmail.com>
2024-01-23 16:17:43 -0300

    fix: btn home

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,9 +18,10 @@ const Banner = () => {
               <span className='text-primary-400'>{titlePart2}</span>
             </h1>
             <p className='max-w-[415px] text-body-md lg:text-body-lg mb-8' data-aos='fade-down' data-aos-delay='600'>{subtitle}</p>
-            <a  href={link} className='btn btn-secondary btn-lg' data-aos='fade-down' data-aos-delay='700' role="button">
+            <button className='btn btn-sm lg:btn-lg btn-secondary' data-aos='fade-down' data-aos-delay='700'>
+              <a href={link}/>
               {textBtn}
-            </a>
+            </button>
           </div>
           {/* imagem */}
           <div 
